9467c5856b829d65e3d4e7c02e25ee02b326dad2
Chisayantando <168519032+Chisayantando@users.noreply.github.com>
2024-07-02 13:36:59 +0200

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const bot = require(__dirname + '/lib/amd')
 const { VERSION } = require(__dirname + '/config')
 
 const start = async () => {
-    Debug.info(`Starting Ednut-Md ${VERSION}`)
+    Debug.info(`Starting Sub-ZeroV1 ${VERSION}`)
   try {
     await bot.init()
     //bot.logger.info('⏳ Database syncing!')
